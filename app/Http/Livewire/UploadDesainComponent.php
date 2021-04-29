064e2091e0528f59da6c971a5bc2ddb87a9dd20e
fikri <fikrialfaninho@gmail.com>
2021-04-29 09:32:26 +0700

    v1

---
+++ app/Http/Livewire/UploadDesainComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class UploadDesainComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.upload-desain-component')->layout('layouts.base');
+    }
+}
---
+++ resources/views/livewire/home-component.blade.php
@@ -0,0 +1,137 @@
+<div id="home" class="slider-area">
+            <div class="bd-example">
+                <div id="carouselOne" class="carousel " data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item bg_cover active" style="background-image: url(assets/images/slider-1.jpg)">
+                            <div class="carousel-caption">
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-xl-6 col-lg-7 col-sm-10">
+                                            <h2 class="carousel-title">DESAIN KAOS ONLINE</h2>
+                                            <h3>Desain Kaos Anda Dengan Mengupload Gambar
+                                            <ul class="carousel-btn rounded-buttons">
+                                                <li><a class="main-btn rounded-three" href="#">Upload Gambarmu</a></li>
+                                              
+                                            </ul>
+                                        </div>
+                                    </div> <!-- row -->
+                                </div> <!-- container -->
+                            </div> <!-- carousel caption -->
+                        </div> <!-- carousel-item -->
+                    </div> <!-- carousel-inner -->
+
+                </div> <!-- carousel -->
+            </div> <!-- bd-example -->
+        </div>
+        <section id="custom" class="pricing-area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section-title text-center pb-20">
+                        <h3 class="title">DESAIN</h3>
+                        <!-- <p class="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p> -->
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row justify-content-center">                
+                <div class="col-lg-4 col-md-7 col-sm-9">
+                    <div class="pricing-style-one mt-40 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.2s">
+                        <div class="pricing-icon text-center">
+                            <img src="assets/images/wman.svg" alt="">
+                        </div>
+                        <div class="pricing-header text-center">
+                            <h5 class="sub-title">DESAIN SENDIRI</h5>
+                            <!-- <p class="month"><span class="price">$ 199</span>/month</p> -->
+                        </div>
+                        <div class="pricing-list">
+                            <ul>
+                                <li><i class="lni-check-mark-circle"></i> Buatlah Desain Sesuai Keinginanmu</li>
+                                <!-- <li><i class="lni-check-mark-circle"></i> Amazing page examples</li> -->
+                            </ul>
+                        </div>
+                        <div class="pricing-btn rounded-buttons text-center">
+                            <a class="main-btn rounded-three" href="#">MULAI DESAIN</a>
+                        </div>
+                    </div> <!-- pricing style one -->
+                </div>
+                
+                <div class="col-lg-4 col-md-7 col-sm-9">
+                    <div class="pricing-style-one mt-40 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+                        <div class="pricing-icon text-center">
+                            <img src="assets/images/wman.svg" alt="">
+                        </div>
+                        <div class="pricing-header text-center">
+                            <h5 class="sub-title">Upload Desain</h5>
+                            <!-- <p class="month"><span class="price">$ 399</span>/month</p> -->
+                        </div>
+                        <div class="pricing-list">
+                            <ul>
+                                <li><i class="lni-check-mark-circle"></i> Unggah Gambar Anda</li>
+                                <!-- <li><i class="lni-check-mark-circle"></i> Amazing page examples</li> -->
+                            </ul>
+                        </div>
+                        <div class="pricing-btn rounded-buttons text-center">
+                            <a class="main-btn rounded-three" href="#">UNGGAH</a>
+                        </div>
+                    </div> <!-- pricing style one -->
+                </div>
+
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    <section id="contact" class="contact-area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section-title text-center pb-20">
+                        <h3 class="title">Get in touch</h3>
+                        <!-- <p class="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p> -->
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="contact-two mt-50 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.2s">
+                        <h4 class="contact-title">Lets talk about the project</h4>
+                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam unde repellendus delectus facilis quia consequatur maxime perferendis! Sequi, modi consequatur.</p>
+                        <ul class="contact-info">
+                            <li><i class="lni-money-location"></i> Elizabeth St, Melbourne, Australia</li>
+                            <li><i class="lni-phone-handset"></i> +333 789-321-654</li>
+                            <li><i class="lni-envelope"></i> hello@ayroui.com</li>
+                        </ul>
+                    </div> <!-- contact two -->
+                </div>
+                <div class="col-lg-6">
+                    <div class="contact-form form-style-one mt-35 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+                        <form  id="contact-form" action="assets/contact.php" method="post">
+                            <div class="form-input mt-15">
+                                <label>Name</label>
+                                <div class="input-items default">
+                                    <input type="text" placeholder="Name" name="name">
+                                    <i class="lni-user"></i>
+                                </div>
+                            </div> <!-- form input -->
+                            <div class="form-input mt-15">
+                                <label>Email</label>
+                                <div class="input-items default">
+                                    <input type="email" placeholder="Email" name="email">
+                                    <i class="lni-envelope"></i>
+                                </div>
+                            </div> <!-- form input -->
+                            <div class="form-input mt-15">
+                                <label>Massage</label>
+                                <div class="input-items default">
+                                    <textarea placeholder="Massage" name="massage"></textarea>
+                                    <i class="lni-pencil-alt"></i>
+                                </div>
+                            </div> <!-- form input -->
+                            <p class="form-message"></p>
+                            <div class="form-input rounded-buttons mt-20">
+                                <button type="submit" class="main-btn rounded-three">Submit</button>
+                            </div> <!-- form input -->
+                        </form>
+                    </div> <!-- contact form -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
